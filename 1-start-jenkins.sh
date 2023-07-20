7d310e043844cc1e9ffb3e1819a68af203a875f9
Pedro Matos <pedro.matos@augmen.pt>
2023-07-20 01:07:04 +0100

    Refactor Jenkins config into folder

--- 1-start-jenkins.sh
+++ 1-start-jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the Docker image and start the Jenkins service
-docker-compose up -d --build
+docker-compose -f jenkins/docker-compose.yml up -d --build
 
 # Print a message to let the user know that Jenkins should be up and running
 echo "Jenkins should now be running on http://localhost:8080"
